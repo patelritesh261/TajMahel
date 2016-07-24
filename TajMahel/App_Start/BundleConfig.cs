25b99a655ee8f6edd13148f55e8bb14a26182835
Ritesh Patel <patelritesh261@outlook.com>
2016-07-24 10:05:11 -0400

    Home page added

--- TajMahel/App_Start/BundleConfig.cs
+++ TajMahel/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace TajMahel
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                     "~/Content/bootstrap.main.css",
+                      "~/Content/business-casual.css",
                       "~/Content/site.css"));
         }
     }
